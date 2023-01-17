6119d172cdd80f8af86ed3fa91d4d9e277db9698
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-01-18 00:21:06 +0400

    Update Homepage.jsx

--- src/page/Homepage/Homepage.jsx
+++ src/page/Homepage/Homepage.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import DashboardHeader from '../../components/dashboard/DashboardHeader'
-import DashboardLayout from '../../layout/DashboardLayout'
-
-const dashboardheaderel = <div style={{display: "flex", justifyContent: "space-between"}}><p></p>
-<p></p>
-</div> 
-
-const nav = <p></p>
-const bodyel = <>
-  <div className="">
-    <DashboardHeader dashboardheaderel={dashboardheaderel} />
-  </div>
-</>
-
+import React from "react";
+import DashboardHeader from "../../components/dashboard/DashboardHeader";
+import DashboardLayout from "../../layout/DashboardLayout";
 
 function Homepage() {
+  const dashboardheaderel = (
+    <div>
+      <p></p>
+      <p></p>
+    </div>
+  );
+
+  const nav = <p></p>;
+  const bodyel = (
+    <>
+      <div className="">
+        <DashboardHeader dashboardheaderel={dashboardheaderel} />
+      </div>
+    </>
+  );
   return (
     <>
-        <div className="">
-            <DashboardLayout navbarelement={nav} bodyelement={bodyel} />
-        </div>
+      <div className="">
+        <DashboardLayout navbarelement={nav} bodyelement={bodyel} />
+      </div>
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
