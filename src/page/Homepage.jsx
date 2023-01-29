f2e31543d51035cf536c5e9d81ad17462ee5f6ba
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-01-29 17:48:54 +0400

    update

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -4,13 +4,50 @@ import {
   arrowdowngray,
   calendarIconSmall,
   clockIcon,
+  doneIcon,
+  moreIcon,
   otherIconTask,
+  peopleAddIcon,
+  shareIcon,
+  taskCalendarIcon,
 } from "../Utils/tools";
 import p2img from "../assets/images/p2.png";
 import p1img from "../assets/images/p1.png";
+import p3img from "../assets/images/p3.png";
 import "./Homepage.css";
 
 function Homepage() {
+  const peoplelist = [p2img, p1img, p3img];
+  const addTaskIcon = (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 12C0.447715 12 0 12.4477 0 13C0 13.5523 0.447715 14 1 14V12ZM25 14C25.5523 14 26 13.5523 26 13C26 12.4477 25.5523 12 25 12V14ZM14 1C14 0.447715 13.5523 0 13 0C12.4477 0 12 0.447715 12 1H14ZM12 25C12 25.5523 12.4477 26 13 26C13.5523 26 14 25.5523 14 25H12ZM1 14H25V12H1V14ZM12 1V25H14V1H12Z"
+        fill="#9A9FBF"
+      />
+    </svg>
+  );
+  const doneTaskIcon = (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 5 5"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M4.82845 0.0617931C5.00354 0.176277 5.05269 0.411026 4.93821 0.586115L2.36247 4.52548C2.30177 4.61833 2.20342 4.67985 2.0934 4.69391C1.98337 4.70797 1.87272 4.67306 1.79064 4.59845L0.123988 3.08332C-0.0308048 2.9426 -0.0422106 2.70304 0.0985092 2.54825C0.239229 2.39346 0.47879 2.38204 0.633583 2.52277L1.97152 3.73907L4.30415 0.171537C4.41864 -0.00355183 4.65336 -0.0526846 4.82845 0.0617931Z"
+        fill="#9A9FBF"
+      />
+    </svg>
+  );
   return (
     <div className="task-manager">
       <div className="left-bar">
@@ -91,7 +128,81 @@ function Homepage() {
           </div>
         </div>
       </div>
-      <div className="right-bar"></div>
+      <div className="right-bar">
+        <div className="hometaskheader">
+          <div className="hometaskheader-left">
+            <div className="hometaskheader-left1">
+              {peoplelist.slice(0, 2).map((peoplelst, key) => (
+                <img
+                  src={peoplelst}
+                  key={key}
+                  alt="img"
+                  className={`hmthlimg${key}`}
+                />
+              ))}
+              <div className="hmthlicon"> {peopleAddIcon}</div>
+            </div>
+            <div className="hmthldone">
+              {doneIcon}
+              <label>MARK AS COMPLETE</label>
+            </div>
+
+            <div className="hmthldate">
+              {taskCalendarIcon}
+              <label>25 days left, Jan 4, 21:52</label>
+            </div>
+          </div>
+
+          <div className="hometaskheader-right">
+            {shareIcon}
+            {moreIcon}
+          </div>
+        </div>
+        <div className="taskwrapper-task">
+          <div className="taskwrapper-taskitem">
+            <p className="taskwrapper-title">Work on the user settings page</p>
+          </div>
+          <div className="taskwrapper-taskitem">
+            <textarea
+              placeholder="Description..."
+              className="twtitextarea"
+            ></textarea>
+          </div>
+          <div className="taskwrapper-taskitem">
+            <div className="twtisubtask">
+              <p className="twtisubtask-title">
+                Subtask <sup>(1)</sup>
+              </p>
+              <div className="twtisubtaskcnt">
+                <div className="twtisubtaskcnt-left">
+                  <img src={p3img} alt="" />
+                  <p>Add some things to the dashboard</p>
+                  <label>5 days left, Jan 4, 21:52</label>
+                </div>
+                <div className="twtisubtaskcnt-right">
+                  <div className="twtisubtaskcnt-check"></div>
+                </div>
+              </div>
+              <div className="twtisubtask_listitem">
+                {doneTaskIcon}
+                <label>Create a new subtask</label>
+              </div>
+            </div>
+          </div>
+          <div className="taskwrapper-taskitem">
+            <div className="twtisubtask">
+              <p className="twtisubtask-title">
+                Checklist <sup>(0/0)</sup>
+              </p>
+              <div className="twticheclist-cnt">
+                {addTaskIcon}
+                <p>Add a new Checklist</p>
+                {peopleAddIcon}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
