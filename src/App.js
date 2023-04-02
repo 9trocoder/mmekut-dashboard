83f127eddea042495f9deb68a4f49e2d9409a762
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-02 18:03:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Homepage from './page/Homepage';
 
-function App() {
-  return (
-    <div>
-      <Homepage />
-    </div>
-  );
-}
+import ErrorPage from './components/error-page';
+import Loginpage from './page/Loginpage';
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "/mmekut",
+    element: <Homepage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/",
+    element: <Loginpage />,
+    errorElement: <ErrorPage />
+  }
+])
+
+export default router;
---
+++ src/page/Loginpage.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import mmekutlogo from "../assets/Svg/mmekutlogo.svg";
+import loginimage from "../assets/images/loginimage.png";
+import { mailIcon, passwordIcon } from "../Utils/tools";
+import "./Loginpage.css";
+import { useNavigate } from "react-router-dom";
+
+function Loginpage() {
+  const navigate = useNavigate();
+  return (
+    <div className="logincnt">
+      <div className="loginleft">
+        <div className="logintop">
+          <img src={mmekutlogo} alt="" />
+        </div>
+        <div className="loginmiddle">
+          <img src={loginimage} alt="" />
+        </div>
+
+        <div className="loginbottom">
+          <p className="loginbottomtext">
+            Built to handle multiple projects across cross-functional teams.
+          </p>
+        </div>
+      </div>
+      <div className="loginright">
+        <div className="logintopp">
+          <img src={mmekutlogo} alt="" />
+        </div>
+        <div className="loginrighttop">
+          <p>Welcome back!!</p>
+          <label>Good to see you back, we have missed you.</label>
+        </div>
+        <div className="loginrightmiddle">
+          <div className="loginitem">
+            <label className="loginrightmiddlelabel">Email</label>
+            <div className="inputcontainer">
+              <div className="inputcontainericon">{mailIcon}</div>
+              <input type="text" placeholder="Enter your email" />
+            </div>
+          </div>
+          <div className="loginitem">
+            <label className="loginrightmiddlelabel">Password</label>
+            <div className="inputcontainer">
+              <div className="inputcontainericon">{passwordIcon}</div>
+              <input type="password" placeholder="Enter password" />
+              <label className="forgotpasswordlabel">
+                Forgot <span>Password</span> ?
+              </label>
+            </div>
+          </div>
+          <button className="login-btn" onClick={() => navigate("/mmekut")}>
+            Log In
+          </button>
+          <div className="donhaveaccount">
+            <p>Don't have an account?</p>
+            <a href="#">Sign up</a>
+          </div>
+        </div>
+
+        <div className="loginrightbottom">
+          <label>?</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loginpage;
