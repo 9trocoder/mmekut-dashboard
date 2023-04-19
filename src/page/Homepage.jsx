eedd44e36c71a04ee2b9f22cebcdc46fe06c6b32
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-19 19:25:41 +0100

    update

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -441,8 +441,8 @@ function Homepage() {
               {showprev && (
                 <button onClick={() => handlePrevious()} className="workonboardprevbtn">
                   <svg
-                    width="30"
-                    height="30"
+                    width="20"
+                    height="20"
                     viewBox="0 0 15 15"
                     fill="#9a9fbf"
                     xmlns="http://www.w3.org/2000/svg"
