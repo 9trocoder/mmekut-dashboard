567cde006996834e00b47ccb59c232129f0cece2
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-19 09:08:52 +0100

    update

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -37,6 +37,10 @@ import Task from "../components/Task";
 import { Link } from "react-router-dom";
 
 function Homepage() {
+  let [incre, setIcre] = useState(1);
+  let [dcre, setDcre] = useState(0);
+  const [shownext, setShowNext] = useState(true);
+  const [showprev, setShowPrev] = useState(false);
   const [alternatetab, setAlternatetab] = useState(false);
   const [rightbartab, setRightbartab] = useState(false);
   const [markdone, setMarkdone] = useState(false);
@@ -148,6 +152,91 @@ function Homepage() {
       ></path>
     </svg>
   );
+
+  const moodname = (
+    <div className="workonboardingmiddle">
+      <p className="workonboardques">Name of your Workspace:</p>
+      <input
+        type="text"
+        autoFocus={true}
+        placeholder="Githarita Workspace"
+        className="workonboardinput"
+      />
+    </div>
+  );
+
+  const customizeworkspace = (
+    <div className="customizeworkspace">
+      <p className="workonboardques">Customize your workspace</p>
+    </div>
+  );
+
+  const workspaceonboardlist = [
+    {
+      id: 1,
+      view: moodname,
+    },
+    {
+      id: 1,
+      view: customizeworkspace,
+    },
+    {
+      id: 1,
+      view: "",
+    },
+    {
+      id: 1,
+      view: "",
+    },
+    {
+      id: 1,
+      view: "",
+    },
+    {
+      id: 1,
+      view: "",
+    },
+    {
+      id: 1,
+      view: "",
+    },
+  ];
+
+  useEffect(() => {
+    if (incre === workspaceonboardlist.length) {
+      setShowNext(false);
+    } else if (incre > workspaceonboardlist.length) {
+      setShowNext(false);
+    }
+
+    if (incre === 1) {
+      setShowPrev(false);
+    }
+  }, [incre, workspaceonboardlist.length]);
+
+  const handleNext = () => {
+    if (incre === workspaceonboardlist.length) {
+      setShowNext(false);
+      setIcre(incre--);
+    } else if (workspaceonboardlist.length > 1) {
+      if (workspaceonboardlist.length > 1) {
+        setShowPrev(true);
+      } else {
+        setShowPrev(false);
+      }
+    } else {
+      setShowNext(true);
+    }
+
+    setIcre((incre = incre + 1));
+    setDcre((dcre = dcre + 1));
+  };
+
+  const handlePrevious = () => {
+    setIcre((incre = incre - 1));
+    setDcre((dcre = dcre - 1));
+    setShowNext(true);
+  };
   return (
     <div className="task-manager">
       {shownav !== "" && (
@@ -346,18 +435,48 @@ function Homepage() {
                 {closeicon}
               </div>
             </div>
-            <div className="workonboardingmiddle">
-              <p className="workonboardques">Name of your Workspace:</p>
-              <input
-                type="text"
-                autoFocus={true}
-                placeholder="Githarita Workspace"
-                className="workonboardinput"
-              />
-            </div>
+            {workspaceonboardlist.slice(dcre, incre).map((wsol, key) => (
+              <div key={key}>{wsol.view}</div>
+            ))}
 
             <div className="workonbardbtnbody">
-              <button className="workonboardbtn">Continue</button>
+              {showprev && (
+                <button onClick={() => handlePrevious()}>
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              )}
+              {shownext && (
+                <button className="workonboardbtn" onClick={() => handleNext()}>
+                  Next
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              )}
             </div>
           </div>
         </>
