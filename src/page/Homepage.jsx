fc97ae230eec8c91b9303417b90cff6787e5fabc
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-19 19:37:25 +0100

    update

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -47,7 +47,7 @@ function Homepage() {
   const [showCalendar, setShowCalendar] = useState(false);
   const [showprofilecard, setprofilecard] = useState(false);
   const [showworkspacecard, setshowworkspacecard] = useState(false);
-  const [showworkspaceonboarding, setshowworkspaceonboarding] = useState(false);
+  const [showworkspaceonboarding, setshowworkspaceonboarding] = useState(true);
   const [shownav, setshownav] = useState("");
   const [homearrow, sethomearrow] = useState("");
   const [showoverlay, setshowoverlay] = useState("");
@@ -153,6 +153,26 @@ function Homepage() {
     </svg>
   );
 
+  const workspacecolor = [
+    "#DA342A",
+    "#AD2089",
+    "#9B62FA",
+    "#826B02",
+    "#0D1C63",
+    "#4B75BF",
+    "#143424",
+    "#6F5345",
+    "#367284",
+    "#443226",
+    "#675D8B",
+    "#172C86",
+    "#991B1A",
+    "#4A2118",
+    "#304B66",
+    "#954A21",
+    "#8B6B49",
+  ];
+
   const moodname = (
     <div className="workonboardingmiddle">
       <p className="workonboardques">Name of your Workspace:</p>
@@ -168,6 +188,38 @@ function Homepage() {
   const customizeworkspace = (
     <div className="customizeworkspace">
       <p className="workonboardques">Customize your workspace</p>
+      <div className="customizeworkspacebody">
+        <div className="customizeworkspaceleft">
+          <p className="customizechooseprofile">Choose your preferred avatar</p>
+          <div className="ccpdrag">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.81825 1.18188C7.64251 1.00615 7.35759 1.00615 7.18185 1.18188L4.18185 4.18188C4.00611 4.35762 4.00611 4.64254 4.18185 4.81828C4.35759 4.99401 4.64251 4.99401 4.81825 4.81828L7.05005 2.58648V9.49996C7.05005 9.74849 7.25152 9.94996 7.50005 9.94996C7.74858 9.94996 7.95005 9.74849 7.95005 9.49996V2.58648L10.1819 4.81828C10.3576 4.99401 10.6425 4.99401 10.8182 4.81828C10.994 4.64254 10.994 4.35762 10.8182 4.18188L7.81825 1.18188ZM2.5 9.99997C2.77614 9.99997 3 10.2238 3 10.5V12C3 12.5538 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2238 12.2239 9.99997 12.5 9.99997C12.7761 9.99997 13 10.2238 13 10.5V12C13 13.104 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2238 2.22386 9.99997 2.5 9.99997Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <p className="ccpdragtext">
+              Drag image here or <span>browse</span>
+            </p>
+          </div>
+          <div className="ccpavatar">
+            <p className="ccpavatartext">E</p>
+          </div>
+        </div>
+        <div className="customizeworkspaceright">
+          {workspacecolor.map((wsc, key) => (
+            <div className="workspacecolordiv" style={{backgroundColor: `${wsc}`}} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 
@@ -416,8 +468,10 @@ function Homepage() {
           <div className="workspaceonboardingoverlay" />
           <div className="workspaceonboardingbody">
             <div className="workonboardlist">
-                <p>{incre} of {workspaceonboardlist.length}</p>
-                {/* <div className="workonboarding_progress">
+              <p>
+                {incre} of {workspaceonboardlist.length}
+              </p>
+              {/* <div className="workonboarding_progress">
                   <div className="workonboardingprog" />
                  <div className="otherwok"></div>
                   <div className="otherwok"></div>
@@ -439,7 +493,10 @@ function Homepage() {
 
             <div className="workonbardbtnbody">
               {showprev && (
-                <button onClick={() => handlePrevious()} className="workonboardprevbtn">
+                <button
+                  onClick={() => handlePrevious()}
+                  className="workonboardprevbtn"
+                >
                   <svg
                     width="20"
                     height="20"
