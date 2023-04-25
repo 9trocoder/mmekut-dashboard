448835a295bc4df7156619ff96cff45fbbc04a01
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-26 00:23:37 +0100

    update

--- src/components/Chathome.jsx
+++ src/components/Chathome.jsx
@@ -36,7 +36,7 @@ function Chathome({
       </div>
 
       <div className="chat-search">
-        <div className="chat-searchicon">{searchIcon}</div>
+        <div className="chat-searchicon">{searchBig}</div>
 
         <input
           type="text"
