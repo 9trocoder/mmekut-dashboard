bc937a8163f1476f54e660962e9cb6a5efde50a0
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-04-28 21:22:23 +0100

    update

--- src/components/Chatview.jsx
+++ src/components/Chatview.jsx
@@ -1,39 +1,35 @@
-import React from 'react'
+import React from "react";
 import p1img from "../assets/images/p1.png";
-import { closeIcon, jother, searchBig } from '../Utils/tools';
-import Leftchat from './Leftchat';
+import { closeIcon, jother, searchBig } from "../Utils/tools";
+import Leftchat from "./Leftchat";
 
-function Chatview({handlechatviewclose, handlechatresoursclick}) {
+function Chatview({ handlechatviewclose, handlechatresoursclick }) {
   return (
     <div className="chatview-bar chatview-bar_res">
-        <div className="chatviewheader">
-            <div className="chatviewheader-left">
-                <div className="chatviewheader-left1">
-                    <div className="rightbarclose"
-                    onClick={handlechatviewclose}
-                    >
-                        {closeIcon}
-                    </div>
-                    <img src={p1img} alt="" />
-                </div>
-                
+      <div className="chatviewheader">
+        <div className="chatviewheader-left">
+          <div className="chatviewheader-left1">
+            <div className="rightbarclose" onClick={handlechatviewclose}>
+              {closeIcon}
             </div>
-            <div className="chatviewheader-right">
-                <div className="chatmoreicon">
-                    {searchBig}
-                </div>
-                <div className="chatmoreicon">
-                    {jother}
-                </div>
-            </div>
+            <img src={p1img} alt="" />
+          </div>
         </div>
-        <div className="chatviewwrapper">
-            <Leftchat message="Hello, we have a meeting by 10" msgtime="11:45 PM" />
-            <Leftchat message="Alright i will fix the meeting to the time is okay for everyone. So the meeting would be on google 
-meeting or zoom meeting. Please Kindly attend all meeting." msgtime="12:00 PM" />
+        <div className="chatviewheader-right">
+          <div className="chatmoreicon">{searchBig}</div>
+          <div className="chatmoreicon">{jother}</div>
         </div>
+      </div>
+      <div className="chatviewwrapper">
+        <Leftchat message="Hello, we have a meeting by 10" msgtime="11:45 PM" />
+        <Leftchat
+          message="Alright i will fix the meeting to the time is okay for everyone. So the meeting would be on google 
+meeting or zoom meeting. Please Kindly attend all meeting."
+          msgtime="12:00 PM"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chatview
+export default Chatview;
