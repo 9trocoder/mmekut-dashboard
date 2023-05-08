2904c1c308ab9fb1e7d952bb2984a7cc6c036a31
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-05-08 20:57:39 +0100

    Update Addtaskscard.jsx

--- src/components/Addtaskscard.jsx
+++ src/components/Addtaskscard.jsx
@@ -5,6 +5,7 @@ import AddtaskAccordionItem from "./AddtaskAccordionItem";
 function Addtaskscard() {
   const [clicked, setClicked] = useState("0");
   const [addtaskshow, setaddtaskshow] = useState(false);
+  const [showfor, setshowfor] = useState(false);
   const handleToggle = (index) => {
     if (clicked === index) {
       return setClicked("0");
@@ -16,12 +17,12 @@ function Addtaskscard() {
       width='15'
       height='15'
       viewBox='0 0 15 15'
-      fill='#df'
+      fill='#fff'
       xmlns='http://www.w3.org/2000/svg'
     >
       <path
         d='M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z'
-        fill='#9a9fbf'
+        fill='#fff'
         fill-rule='evenodd'
         clip-rule='evenodd'
       ></path>
@@ -99,9 +100,54 @@ function Addtaskscard() {
                     <div className='addtcbforprojectassign'>
                       {peopleAddIcon}
                     </div>
+        <div className='addtakscardoverlay' />
+        <div className='addtakscardbody'>
+          <div className='addtakscardbodycnt'>
+            <div className='addtcbctop'>
+              <div className='addtcbctopleft'>
+                <textarea
+                  rows={1}
+                  type='text'
+                  autoFocus={true}
+                  placeholder='Make a summary of your task...'
+                  className='addtcbctopletinput'
+                />
+              </div>
+              <div className='addtcbctopright'>
+                <div className='addtcbctoprightclosebtn'>{closeicon}</div>
+              </div>
+            </div>
+            <div className='addtcbcfor'>
+              <div className='addtcbcforproject'>
+                <p className='addtcbcforprojecttitle'>For</p>
+                <div className='addtcbforprojectadd'>
+                  <div className='' onClick={() => setshowfor(true)}>
+                    {addtaskicon}
                   </div>
+
+                  {showfor && (
+                    <>
+                      <div
+                        className='addtcbforcntoverlay'
+                        onClick={() => setshowfor(false)}
+                      />
+                      <div className='addtcbforcnt'>
+                        <ul className='addtchcaccordingcnt'>
+                          {addtaskaccordlist.map((list, index) => (
+                            <AddtaskAccordionItem
+                              onToggle={() => handleToggle(index)}
+                              key={index}
+                              list={list}
+                              active={clicked === index}
+                            />
+                          ))}
+                        </ul>
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
+            </div>
             </div>
           </>
         )}
