d42f2176b1fbb4bcb7199fabc9ea6e0a8f255d44
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-05-24 17:58:47 +0100

    update

--- src/components/Documenthome.jsx
+++ src/components/Documenthome.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function Documenthome({ homearrow, handledisplay }) {
   const [activeheaderbtn, setactiveheaderbtn] = useState("btn-1");
+  const [activecatbtn, setactivecatbtn] = useState("btn-1");
   const documentheaderbtnlist = [
     {
       id: "btn-1",
@@ -20,6 +21,17 @@ function Documenthome({ homearrow, handledisplay }) {
     {
       id: "btn-4",
       title: "Private",
+    },
+  ];
+
+  const documentcatbtn = [
+    {
+      id: "btn-1",
+      title: "Notes",
+    },
+    {
+      id: "btn-2",
+      title: "Personal tasks",
     },
   ];
   return (
@@ -51,6 +63,19 @@ function Documenthome({ homearrow, handledisplay }) {
             </button>
           ))}
         </div>
+      </div>
+      <div className='homesecheader'>
+        {documentcatbtn.map((dcblist, index) => (
+          <button
+            className={
+              activecatbtn === dcblist.id ? "activecatbtn" : "notactivecatbtn"
+            }
+            key={index}
+            onClick={() => setactivecatbtn(dcblist.id)}
+          >
+            {dcblist.title}
+          </button>
+        ))}
       </div>
     </div>
   );
